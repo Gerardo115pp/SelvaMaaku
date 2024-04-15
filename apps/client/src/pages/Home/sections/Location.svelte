fa4c4ba02dbde93fa51bc5785530c82119f8a845
Gerardo <theronin115@gmail.com>
2024-04-15 01:17:24 -0600

    Finished the maps animation on the Location section of the home page

--- apps/client/src/pages/Home/sections/Location.svelte
+++ apps/client/src/pages/Home/sections/Location.svelte
@@ -1,44 +1,186 @@
 <script>
     import ThemeButton from "@components/buttons/theme_button.svelte";
-import Arrow from "@components/icons/arrow.svelte";
     import Mexico from "@components/icons/mexico.svelte";
     import SayulitaMap from "@components/icons/sayulita_map.svelte";
     import WixaricaIcon from "@components/icons/wixarica_icon.svelte";
+    import viewport from "@components/viewport_actions/useViewportActions";
+    import { blur, fly } from "svelte/transition";
+
+    
+    /*=============================================
+    =            Properties            =
+    =============================================*/
+    
+        /**
+         * whether the component is currently visible
+         * @type {boolean}
+         */
+        let component_visible = false;
+
+        
+        /*----------  Animation properties  ----------*/
+
+            /**
+             * Animation total duration in milliseconds
+             * @type {number}
+             */
+            const section_animation_duration = 6000;
+
+            /**
+             * Mexico map animation duration percentage
+             * @type {number}
+             */
+            const mexico_animation_duration = (section_animation_duration * 0.4);
+
+            /**
+             * Mexico map visible
+             * @type {boolean}
+             */
+            let mexico_map_visible = false;
+
+            /**
+             * Sayulita map animation duration percentage
+             * @type {number}
+             */
+            const sayulita_animation_duration = section_animation_duration * 0.4;
+
+            /**
+             * Sayulita map visible
+             * @type {boolean}
+             */
+            let sayulita_map_visible = false;
+
+            /**
+             * Is location content visible
+             * @type {boolean}
+             */
+            let location_content_visible = false;
+            
+            /**
+             * Location content animation duration
+             * @type {number}
+             */
+            const location_content_animation_duration = section_animation_duration * 0.3;
+
+            /**
+             * The animation elements that need to be dimmed
+             * @type {HTMLElement[]}
+             */
+            const dimmed_elements = [];
+
+
+        
+        
+    
+    /*=====  End of Properties  ======*/
+    
+
+    
+    /*=============================================
+    =            Methods            =
+    =============================================*/
+
+        /**
+         * Dims the elements that need to be dimmed
+         */
+        const dimAnimationElements = () => {
+            dimmed_elements.forEach((element) => {
+                element.style.opacity = 0.3;
+            });
+        }
+
+        /**
+         * Manages the appearance of the animation components
+        */
+        const manageAnimation = () => {
+            mexico_map_visible = true;
+
+            if (component_visible) {
+                setTimeout(() => {
+                    sayulita_map_visible = true;
+
+                    setTimeout(() => {
+                        location_content_visible = true;
+                        dimAnimationElements();
+                    }, sayulita_animation_duration);
+
+                }, mexico_animation_duration);
+            }
+        }
+
+        /**
+         * Registers an animation element to be dimmed
+         * @param {HTMLElement} node
+         */
+        const registerDimmedElement = (node) => {
+            dimmed_elements.push(node);
+        }
+    
+        /**
+         * sets the component visibility
+         * @param {boolean} is_visible
+         */
+        const setComponentVisibility = (is_visible) => {
+            component_visible = is_visible;
+
+            if (is_visible) {
+                manageAnimation();
+            }
+        }
+    
+    /*=====  End of Methods  ======*/
+    
+    
+    
 </script>
 
-<section id="smk-home-location-section" class:adebug={false}>
+<section id="smk-home-location-section" class:adebug={false} on:viewportEnter={() => setComponentVisibility(true)} use:viewport>
     <div id="smk-hls-content-wrapper" class="design-content-width">
-        <div id="smk-hls-cw-mexico-map-wrapper" class="smk-hls-bg-element">
-            <Mexico
-                mexico_opacity={0.4}
-                map_pin_opacity={0.3}
-            />
-        </div>
-        <div id="smk-hls-cw-sayulita-map-wrapper" class="smk-hls-bg-element">
-            <SayulitaMap />
-        </div>
-        <div id="smk-hls-cw-content">
-            <div class="smk-hls-cw-top-icon wixarica-icons">
-                <WixaricaIcon with_accent />
+        {#key mexico_map_visible}        
+            <div id="smk-hls-cw-mexico-map-wrapper" class="smk-hls-bg-element" style:transition="opacity {location_content_animation_duration * 0.2}ms ease" in:fly={{x: -400, duration: mexico_animation_duration}} use:registerDimmedElement>
+                {#if mexico_map_visible}
+                    <Mexico
+                        mexico_opacity={0.4}
+                        map_pin_opacity={0.3}
+                        animation_duration={mexico_animation_duration * 1.2}
+                        animation_delay={mexico_animation_duration * 0.3}
+                        animate
+                    />
+                {/if}
             </div>
-            <hgroup id="smk-hls-cw-sayulita-information">
-                <h2 id="smk-hls-cw-sayulita-headline">
-                    SAYULITA
-                </h2>
-                <h3 id="smk-hls-cw-sayulita-subheadline">
-                    The spectacular Hidden Gem of the Riviera Nayarit
-                </h3>
-                <p id="smk-hls-cw-sayulita-text">
-                    Sayulita, Mexico, is a vibrant and picturesque small town located on the Pacific coast in the state of Nayarit. Known for its stunning beaches, relaxed atmosphere, and rich cultural heritage.
-                </p>
-            </hgroup>
-            <ul class="smk-hls-cw-ctas-wrapper">
-                <ThemeButton 
-                    href="/location"
-                    text="Location"
+        {/key}
+        <div id="smk-hls-cw-sayulita-map-wrapper" class="smk-hls-bg-element" style:transition="opacity {location_content_animation_duration * 0.2}ms ease" use:registerDimmedElement>
+            {#if sayulita_map_visible}
+                <SayulitaMap 
+                    animation_duration={sayulita_animation_duration}
+                    animate
                 />
-            </ul>
+            {/if}
         </div>
+        {#if location_content_visible}
+             <div id="smk-hls-cw-content" in:blur={{amount: 1.5, duration: location_content_animation_duration}}>
+                 <div class="smk-hls-cw-top-icon wixarica-icons">
+                     <WixaricaIcon with_accent />
+                 </div>
+                 <hgroup id="smk-hls-cw-sayulita-information">
+                     <h2 id="smk-hls-cw-sayulita-headline">
+                         SAYULITA
+                     </h2>
+                     <h3 id="smk-hls-cw-sayulita-subheadline">
+                         The spectacular Hidden Gem of the Riviera Nayarit
+                     </h3>
+                     <p id="smk-hls-cw-sayulita-text">
+                         Sayulita, Mexico, is a vibrant and picturesque small town located on the Pacific coast in the state of Nayarit. Known for its stunning beaches, relaxed atmosphere, and rich cultural heritage.
+                     </p>
+                 </hgroup>
+                 <ul class="smk-hls-cw-ctas-wrapper">
+                     <ThemeButton 
+                         href="/location"
+                         text="Location"
+                     />
+                 </ul>
+             </div>
+        {/if}
     </div>
 </section>
 
